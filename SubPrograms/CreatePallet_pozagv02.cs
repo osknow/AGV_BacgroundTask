cb36fa4d4fb18ab69c6cdecfd0d8039003fbbf13
osknow <osknow@POZ230017>
2025-05-27 14:21:26 +0200

    Added coment to conole write to beter see what hapend when we reset ora set place with palet rosurce.

--- SubPrograms/CreatePallet_pozagv02.cs
+++ SubPrograms/CreatePallet_pozagv02.cs
@@ -34,7 +34,7 @@ namespace AGV_BackgroundTask.SubPrograms
                 }
                 else
                 {
-                    Console.WriteLine($"{response.StatusCode} , {response.RequestMessage}");
+                    Console.WriteLine($"{response.StatusCode} , {response.RequestMessage}, Id punktu: {data.symbolicPointId} , typ: {data.resourceType}, ilość: {data.amount}");
                 }
                 }
                 catch(Exception e)
